c7b8b01219953224e6a1a89801b2e209ae4242c5
qerdcv <vadym.tishchenko2@gmail.com>
2024-12-02 12:04:21 +0200

    Add benchmark results

--- days/day_2/day_2_test.go
+++ days/day_2/day_2_test.go
@@ -2,6 +2,7 @@ package day2
 
 import "testing"
 
+// BenchmarkResolveIsSaveV1-12    	1000000000	         1.192 ns/op	       0 B/op	       0 allocs/op
 func BenchmarkResolveIsSaveV1(b *testing.B) {
 	input := []int{80, 80, 79, 73, 71, 67}
 
@@ -10,10 +11,11 @@ func BenchmarkResolveIsSaveV1(b *testing.B) {
 	}
 }
 
+// BenchmarkResolveIsSaveV2-12    	20447596	        57.04 ns/op	      48 B/op	       1 allocs/op
 func BenchmarkResolveIsSaveV2(b *testing.B) {
 	input := []int{80, 80, 79, 73, 71, 67}
 
 	for range b.N {
-		isSave(input)
+		isSaveV2(input)
 	}
 }
