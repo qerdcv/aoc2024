4616e2cc7f5754e5a034037b6cd021ceeb3f318f
qerdcv <vadym.tishchenko2@gmail.com>
2024-12-12 09:57:05 +0200

    Solve y2024d12p2

--- 2024/12/part_two.go
+++ 2024/12/part_two.go
@@ -6,46 +6,54 @@ import (
 	"github.com/qerdcv/aoc2024/internal/generic"
 )
 
+type edge struct {
+	p  pos
+	dp pos
+}
+
+var dps = []pos{
+	{-1, 0},
+	{1, 0},
+	{0, 1},
+	{0, -1},
+}
+
+func bounds(p pos, gridSize int) bool {
+	return p.y >= 0 && p.y < gridSize && p.x >= 0 && p.x < gridSize
+}
+
+func scanRegion(p pos, ch string, grid [][]string, visited [][]bool, edges map[edge]struct{}, gridSize int) int {
+	if visited[p.y][p.x] || grid[p.y][p.x] != ch {
+		return 0
+	}
+
+	visited[p.y][p.x] = true
+	area := 1
+
+	// Explore neighboring cells
+	for _, dp := range dps {
+		newP := pos{y: p.y + dp.y, x: p.x + dp.x}
+
+		if !bounds(newP, gridSize) || grid[newP.y][newP.x] != ch {
+			edges[edge{p: p, dp: dp}] = struct{}{}
+		} else {
+			area += scanRegion(newP, ch, grid, visited, edges, gridSize)
+		}
+	}
+
+	return area
+}
+
 func solvePartTwo(input string) (int, error) {
 	grid := generic.Map(strings.Split(input, "\n"), func(t string) []string {
 		return strings.Split(t, "")
 	})
-	gL := len(grid)
+	gridSize := len(grid)
 
-	visited := make([][]bool, gL)
+	// Initialize visited array
+	visited := make([][]bool, gridSize)
 	for i := range visited {
-		visited[i] = make([]bool, gL)
-	}
-
-	bound := func(p pos) bool {
-		return p.y < gL && p.y >= 0 && p.x < gL && p.x >= 0
-	}
-
-	var scanRegion func(p pos, ch string, edges map[pos]struct{}) int
-
-	scanRegion = func(p pos, ch string, edges map[pos]struct{}) int {
-		if visited[p.y][p.x] || grid[p.y][p.x] != ch {
-			return 0
-		}
-
-		visited[p.y][p.x] = true
-		area := 1
-		for _, dp := range []pos{
-			{-1, 0},
-			{1, 0},
-			{0, 1},
-			{0, -1},
-		} {
-			newP := pos{y: p.y + dp.y, x: p.x + dp.x}
-			if !bound(newP) || grid[newP.y][newP.x] != ch {
-				edges[newP] = struct{}{}
-				continue
-			}
-
-			area += scanRegion(newP, ch, edges)
-		}
-
-		return area
+		visited[i] = make([]bool, gridSize)
 	}
 
 	total := 0
@@ -56,10 +64,18 @@ func solvePartTwo(input string) (int, error) {
 				continue
 			}
 
-			// TODO: count sides
-			edges := map[pos]struct{}{}
-			a := scanRegion(p, ch, edges)
-			total += a
+			edges := make(map[edge]struct{})
+			area := scanRegion(p, string(ch), grid, visited, edges, gridSize)
+
+			dupEdges := 0
+			for e := range edges {
+				reverseEdge := edge{p: pos{y: e.p.y + e.dp.x, x: e.p.x + e.dp.y}, dp: e.dp}
+				if _, exists := edges[reverseEdge]; exists {
+					dupEdges++
+				}
+			}
+
+			total += area * (len(edges) - dupEdges)
 		}
 	}
 
